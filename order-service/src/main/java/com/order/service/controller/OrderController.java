905965ea895230d4665b5ef55e9c44ad5c7996c3
huangsen <1538356302@qq.com>
2020-07-07 23:33:43 +0800

    通过LoadBalancerClient进行负载均衡

--- order-service/src/main/java/com/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/order/service/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.order.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +13,9 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/{id}")
     public String getOrder(@PathVariable("id") Long id) {
         return "Order:" + id;
@@ -18,7 +23,11 @@ public class OrderController {
 
     @PostMapping("/place")
     public String placeOrder(@RequestParam("userId") Long userId, @RequestParam("orderId") Long orderId) {
-        String user = restTemplate.getForEntity("http://127.0.0.1:8080/user/" + userId, String.class).getBody();
+        ServiceInstance serviceInstance = loadBalancerClient.choose("user-service");
+        //获取服务实例
+        String serviceUrl = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort());
+        //执行Get请求
+        String user = restTemplate.getForEntity(serviceUrl + "/user/" + userId, String.class).getBody();
         String order = getOrder(orderId);
         return String.format("用户%s,对商品%s进行了下单", user, order);
     }
--- user-service/src/main/java/com/user/service/controller/UserController.java
+++ user-service/src/main/java/com/user/service/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.user.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/{id}")
     public String getUser(@PathVariable("id") Long id) {
+        System.out.println(port);
         return "User[" + id + "]";
     }
 }
